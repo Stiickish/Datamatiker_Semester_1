82dfb56768c6ea0ae7c2939f6a15c37917d6d24d
Stiickish <yelong.he@hotmail.com>
2022-01-04 20:51:17 +0100

    updates

--- Exercises/EksamensopgaveStenSaksPapir/StenSaksPapir.java
+++ Exercises/EksamensopgaveStenSaksPapir/StenSaksPapir.java
@@ -25,61 +25,52 @@ public class StenSaksPapir {
 
         int spiller = 0;
         int computer = 0;
-        int runde = 0;
         int uafgjort = 0;
         int computerSvar = Integer.valueOf(computerValg());
         String brugerValgSvar = brugerValg();
 
-        //while (runde < 3) {
-
-            switch (brugerValgSvar) {
-                case "sten":
-                    if (computerSvar == 0) {
-                        System.out.println("Tie!");
-                        uafgjort++;
-                    } else if (computerSvar == 1) {
-                        System.out.println("Computer win!");
-                        computer++;
-                    } else {
-                        System.out.println("Player win!");
-                        spiller++;
-                        runde++;
-                    }
-                    break;
-                case "saks":
-                    if (computerSvar == 0) {
-                        System.out.println("Player win!");
-                        spiller++;
-                        runde++;
-                    } else if (computer == 1) {
-                        System.out.println("Tie!");
-                        uafgjort++;
-                    } else {
-                        System.out.println("Computer win!");
-                        computer++;
-                    }
-                    break;
-                case "papir":
-                    if (computer == 0) {
-                        System.out.println("Computer win!");
-                        computer++;
-                    } else if (computer == 1) {
-                        System.out.println("Player win!");
-                        spiller++;
-                        runde++;
-                    } else {
-                        System.out.println("Tie!");
-                        uafgjort++;
-                    }
-                    break;
-                default:
-                    System.out.println("I only recognise sten, saks or papir");
-                    //spil();
-                    break;
-
-            //}
+        switch (brugerValgSvar) {
+            case "sten":
+                if (computerSvar == 0) {
+                    System.out.println("Tie!");
+                    uafgjort++;
+                } else if (computerSvar == 1) {
+                    System.out.println("Computer win!");
+                    computer++;
+                } else {
+                    System.out.println("Player win!");
+                    spiller++;
+                }
+                break;
+            case "saks":
+                if (computerSvar == 0) {
+                    System.out.println("Player win!");
+                    spiller++;
+                } else if (computer == 1) {
+                    System.out.println("Tie!");
+                    uafgjort++;
+                } else {
+                    System.out.println("Computer win!");
+                    computer++;
+                }
+                break;
+            case "papir":
+                if (computer == 0) {
+                    System.out.println("Computer win!");
+                    computer++;
+                } else if (computer == 1) {
+                    System.out.println("Player win!");
+                    spiller++;
+                } else {
+                    System.out.println("Tie!");
+                    uafgjort++;
+                }
+                break;
+            default:
+                System.out.println("I only recognise sten, saks or papir");
+                spil();
+                break;
         }
-        System.out.println("");
         System.out.println("Spilleren har vundet:  " + spiller);
         System.out.println("Computeren har vundet: " + computer);
         System.out.println("Antal uafgjorte kampe: " + uafgjort);
